e13b5f508b76ce80692ee407540d68274b2c7d79
Vinicius da Silva Montibeller <vinism96@hotmail.com>
2023-03-14 20:31:05 -0300

    adjusting js file direction

--- src/scripts/services/repositories.js
+++ src/scripts/services/repositories.js
@@ -1,4 +1,4 @@
-import { baseUrl, reposAndEventsQuantity } from "/src/scripts/variables.js"
+import { baseUrl, reposAndEventsQuantity } from "../variables.js"
 
 async function getRepositories(userName){
     const response = await fetch(`${baseUrl}/${userName}/repos?per_page=${reposAndEventsQuantity}`)
--- src/scripts/variables.js
+++ src/scripts/variables.js
@@ -1,4 +1,4 @@
-const baseUrl = 'https://api.github.com/users'
+const baseUrl = "https://api.github.com/users"
 const reposAndEventsQuantity = 10
 
 export { baseUrl, reposAndEventsQuantity }
